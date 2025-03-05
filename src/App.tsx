c1abb50b627e1659a12184fdfdc686d745bb8254
meltemozen <meltem.ozen.1999@gmail.com>
2025-03-05 16:48:30 +0300

    components updated

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from "./components/Navbar";
+import MainPage from "./components/MainPage";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+    <Navbar title={""}/>
+
+    <MainPage title={""}/>
+
+    <Footer title={""}/>
+    </>
+  )
+}
+
+export default App
