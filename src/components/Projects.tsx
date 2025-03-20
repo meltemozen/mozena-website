93a8467182dc4e9022e1f41a02f99078a8194e84
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2025-03-20 14:27:22 +0300

    feat: Add animation on scroll

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState, useRef, Fragment } from "react"
+
 interface ProjectsProps {
   title: string
 }
@@ -37,15 +39,59 @@ const Projects: React.FC<ProjectsProps> = ({ title }) => {
     >
       <h1 className="text-3xl font-bold">{title}</h1>
       <ul className="flex flex-wrap justify-center gap-12 md:gap-24">
-        {projectsData.map((project, i) => (
-          <li key={`p_${i}`} className="w-full md:w-[40%] flex flex-col gap-2">
-            <div className="h-[300px] bg-neutral-500 rounded-3xl mb-2"></div>
-            <h2 className="text-xl font-bold uppercase">{project.title}</h2>
-            <p className="text-lg">{project.description}</p>
-          </li>
+        {projectsData.map((project, index) => (
+          <Fragment key={`p_${index}`}>
+            <ProjectItem project={project} index={index} />
+          </Fragment>
         ))}
       </ul>
     </section>
+  )
+}
+
+const ProjectItem = ({ project, index }: { project: any; index: number }) => {
+  const ref = useRef<HTMLLIElement>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+          observer.unobserve(entry.target)
+        }
+      },
+      { threshold: 0.3 }
+    )
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+
+    return () => observer.disconnect()
+  }, [])
+
+  return (
+    <li
+      className="relative w-full md:w-[40%] flex flex-col gap-2"
+      style={{
+        transition: "all 1.25s",
+        opacity: isVisible ? 1 : 0,
+        transform: isVisible
+          ? "translateX(0)"
+          : index % 2 === 0
+          ? "translateX(-150px)"
+          : "translateX(150px)",
+      }}
+      ref={ref}
+    >
+      {/* Project Image */}
+      <div className="h-[300px] bg-neutral-500 rounded-3xl mb-2" />
+
+      {/* Project Title & Description */}
+      <h2 className="text-xl font-bold uppercase">{project.title}</h2>
+      <p className="text-lg">{project.description}</p>
+    </li>
   )
 }
 
