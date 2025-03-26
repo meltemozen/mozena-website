abe2c872513405fdd357266d2ff1276fcc9c0e07
Emre Gundogdu <127036302+emreguendogdu@users.noreply.github.com>
2025-03-26 10:50:21 +0300

    feat: Add MouseTracker, also shows an arrow on Projects' hover (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ import CommunicationForm from './components/CommunicationForm';
 import Blog from './components/Blog';
 import Services from './components/Services';
 import Projects from './components/Projects';
-import background_items from "./assets/Background items.png"
+import background_items from "./assets/Background items.png";
 import rocketImage from './assets/Uzay.png';
 import Blogimages from "./assets/blog.png";
+import MouseTracker from './components/MouseTracker';
 
 function App() {
   return (
     <>
 
     <Navbar title={"Navbar"}/>
-    <About title={"Güvenilir ve Sonuç Odaklı"}  background_items= {background_items}/>
+    <About title={"Güvenilir ve Sonuç Odaklı"}  background_items={background_items} />
     <Projects title={"Projects"}/>
     <Services servicesTitle={"Hizmetlerimiz"} rocketImage={rocketImage} mainTitle1={"UI/UX Design"} subTitle1={"Velit dolor enim pariatur excepteur commodo mollit commodo qui pariatur esse consectetur. Elit proident et proident est anim et fugiat consectetur reprehenderit veniam dolore ut sint aliqua eu."}/>
     <Blog title={"Blog"} blogimage={Blogimages}/>
     <CommunicationForm title={"CommunicationForm"}/>   
-    <Footer title={"Footer"}/>
+    <Footer title={"Footer"} />
+    <MouseTracker />
     </>
   )
 };
